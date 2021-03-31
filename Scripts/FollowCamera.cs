2dd44258f9f45dc141a2bcc9cfc7a8794b8fbd27
SeoulP <55219779+SeoulP@users.noreply.github.com>
2021-03-31 15:36:09 -0500

    Add files via upload

---
+++ Scripts/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cam;
+    [SerializeField]
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((cam.transform.position - transform.position).magnitude > 550)
+        {
+            transform.position += new Vector3(0, 0, 1000 + Random.Range(-50, 51));
+        }
+
+        if (gameObject.CompareTag("Bird"))
+        {
+            if ((player.transform.position - transform.position).magnitude > 550)
+            {
+                transform.position = new Vector3(0, player.transform.position.y + Random.Range(-80, 80), player.transform.position.z + Random.Range(325, 550));
+            }
+        }
+    }
+}
